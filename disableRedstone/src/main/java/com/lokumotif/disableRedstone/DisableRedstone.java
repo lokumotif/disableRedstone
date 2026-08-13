391e98cfb97d9ea8637885e6b4b220663c4d9bc5
lokumotif <miridinyo@gmail.com>
2026-08-13 14:51:57 +0300

    Add files via upload

---
+++ disableRedstone/src/main/java/com/lokumotif/disableRedstone/DisableRedstone.java
@@ -0,0 +1,19 @@
+package disableRedstone.disableRedstone;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableRedstone extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onRedstone(BlockRedstoneEvent event) {
+        event.setNewCurrent(0);
+    }
+}
