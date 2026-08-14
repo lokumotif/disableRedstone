ad1e56772d0d9b10d9aaac6d54eb4f4d432e5f96
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-15 05:01:08 +1000

    invert redstone component interaction toggle check

--- src/main/java/com/lokumotif/disableRedstone/DisableRedstone.java
+++ src/main/java/com/lokumotif/disableRedstone/DisableRedstone.java
@@ -243,7 +243,7 @@ public final class DisableRedstone extends JavaPlugin implements Listener, Comma
         }
     
         // Disable Redstone components
-        if (getConfig().getBoolean("features.redstone")
+        if (!getConfig().getBoolean("features.redstone")
                 && isRedstoneComponent(block.getType())) {
             event.setCancelled(true);
         }
