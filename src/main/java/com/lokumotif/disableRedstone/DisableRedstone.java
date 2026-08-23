68bc164eb6f236f5147adb3050a2125e61bf3039
lokumotif <miridinyo@gmail.com>
2026-08-23 03:45:54 +0300

    Invert feature checks for jukebox and redstone lamp

--- src/main/java/com/lokumotif/disableRedstone/DisableRedstone.java
+++ src/main/java/com/lokumotif/disableRedstone/DisableRedstone.java
@@ -426,7 +426,7 @@ public final class DisableRedstone extends JavaPlugin implements Listener, Comma
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
     public void onJukeboxInteract(PlayerInteractEvent event) {
     
-        if (!getConfig().getBoolean("features.jukebox")) {
+        if (getConfig().getBoolean("features.jukebox")) {
             return;
         }
     
@@ -456,13 +456,13 @@ public final class DisableRedstone extends JavaPlugin implements Listener, Comma
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = false)
     public void onLampPhysics(BlockPhysicsEvent event) {
     
-        if (!getConfig().getBoolean("features.redstone-lamp")) {
+        if (getConfig().getBoolean("features.redstone-lamp")) {
             return;
         }
     
         Block block = event.getBlock();
     
-        if (isRedstoneLamp(block.getType())) {
+        if (!isRedstoneLamp(block.getType())) {
             return;
         }
     
